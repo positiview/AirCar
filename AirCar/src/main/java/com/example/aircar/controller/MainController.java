84ebece15cd432a0bb79474e7d18408caa8d48f6
positiview <goaud2022@gmail.com>
2023-09-04 14:31:55 +0900

    커밋 230904

--- AirCar/src/main/java/com/example/aircar/controller/MainController.java
+++ AirCar/src/main/java/com/example/aircar/controller/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
     public String homepage(){
         return "main/homepage";
     }
+
+    @GetMapping("/index")
+    public String slickPractice(){
+        return "index";
+    }
 }
