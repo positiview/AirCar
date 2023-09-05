87db9e3af2474530c55f0328b7ce962bc84ae923
positiview <goaud2022@gmail.com>
2023-09-05 13:55:32 +0900

    메인페이지 헤더 수정

--- AirCar/src/main/java/com/example/aircar/controller/MemberController.java
+++ AirCar/src/main/java/com/example/aircar/controller/MemberController.java
@@ -1,20 +1,4 @@
 package com.example.aircar.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/member")
 public class MemberController {
-
-    @GetMapping("/login")
-    public String memberLogin(){
-        return "member/memberLogin";
-    }
-
-    @GetMapping("/signUpAgree")
-    public String signUpAgree(){
-        return "member/signUpAgreeChk";
-    }
 }
