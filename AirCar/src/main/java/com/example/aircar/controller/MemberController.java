ecf49a4d2d1ca6732f88cb4526e681124ec2e9c4
positiview <goaud2022@gmail.com>
2023-09-07 14:21:16 +0900

    커밋

--- AirCar/src/main/java/com/example/aircar/controller/MemberController.java
+++ AirCar/src/main/java/com/example/aircar/controller/MemberController.java
@@ -51,6 +51,11 @@ public class MemberController {
 
     }
 
+    @GetMapping("/adminTest")
+    public String admin(){
+        return "member/AdminLogin";
+    }
+
     /*@GetMapping("/login/error")
     public String loginError(Model model){
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요.");
